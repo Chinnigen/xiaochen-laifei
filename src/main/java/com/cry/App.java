e35d64befba3342cb680f243e6afdaae303af3e2
chenriyuan <chenriyuan@gmail.com>
2020-03-23 09:35:11 +0800

    no message

--- src/main/java/com/cry/App.java
+++ src/main/java/com/cry/App.java
@@ -1,5 +1,7 @@
 package com.cry;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import tk.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,8 +16,15 @@ import org.springframework.cache.annotation.EnableCaching;
 @SpringBootApplication
 @EnableCaching
 @MapperScan("com.cry.dao")
-public class App {
+public class App /*extends SpringBootServletInitializer*/ {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
+
+    /*
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
+    }
+    */
 }
